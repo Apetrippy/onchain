524d2f8db99c1d8fb6bfa9ddd57c77e27003ae56
DonnGregoor69 <88169436+DonnGregoor69@users.noreply.github.com>
2023-06-04 05:03:40 +0200

    update to V2

---
+++ .github/components/buttons/BuyNow.tsx
@@ -0,0 +1,86 @@
+import React, { ComponentProps, FC, ReactNode } from 'react'
+import { SWRResponse } from 'swr'
+import { useNetwork, useSigner } from 'wagmi'
+import { BuyModal, BuyStep, useTokens } from '@reservoir0x/reservoir-kit-ui'
+import { useSwitchNetwork } from 'wagmi'
+import { Button } from 'components/primitives'
+import { useConnectModal } from '@rainbow-me/rainbowkit'
+import { CSS } from '@stitches/react'
+import { useMarketplaceChain } from 'hooks'
+
+type Props = {
+  tokenId?: string
+  collectionId?: string
+  orderId?: string
+  buttonCss?: CSS
+  buttonProps?: ComponentProps<typeof Button>
+  buttonChildren?: ReactNode
+  mutate?: SWRResponse['mutate']
+}
+
+const BuyNow: FC<Props> = ({
+  tokenId,
+  collectionId,
+  orderId = undefined,
+  mutate,
+  buttonCss,
+  buttonProps = {},
+  buttonChildren,
+}) => {
+  const { data: signer } = useSigner()
+  const { openConnectModal } = useConnectModal()
+  const { chain: activeChain } = useNetwork()
+  const marketplaceChain = useMarketplaceChain()
+  const { switchNetworkAsync } = useSwitchNetwork({
+    chainId: marketplaceChain.id,
+  })
+  const isInTheWrongNetwork = Boolean(
+    signer && activeChain?.id !== marketplaceChain.id
+  )
+
+  const trigger = (
+    <Button css={buttonCss} color="primary" {...buttonProps}>
+      {buttonChildren}
+    </Button>
+  )
+  const canBuy = signer && tokenId && collectionId && !isInTheWrongNetwork
+
+  return !canBuy ? (
+    <Button
+      css={buttonCss}
+      aria-haspopup="dialog"
+      color="primary"
+      onClick={async () => {
+        if (isInTheWrongNetwork && switchNetworkAsync) {
+          const chain = await switchNetworkAsync(marketplaceChain.id)
+          if (chain.id !== marketplaceChain.id) {
+            return false
+          }
+        }
+
+        if (!signer) {
+          openConnectModal?.()
+        }
+      }}
+      {...buttonProps}
+    >
+      {buttonChildren}
+    </Button>
+  ) : (
+    <BuyModal
+      trigger={trigger}
+      tokenId={tokenId}
+      collectionId={collectionId}
+      orderId={orderId}
+      //CONFIGURABLE: set any fees on top of orders, note that these will only
+      // apply to native orders (using the reservoir order book) and not to external orders (opensea, blur etc)
+      referrer={"0x9724bbbdA61A23BaDE2db83eFd847dE65e2E6cab"}
+      referrerFeeBps={69}
+      onClose={(data, stepData, currentStep) => {
+        if (mutate && currentStep == BuyStep.Complete) mutate()
+      }}
+    />
+  )
+}
+
+export default BuyNow
